0a0564152bde6a68b811862851480c7419a6d506
i-qbu <iqbu.lab@gmail.com>
2024-01-17 22:03:29 +0900

    Initial Commit

---
+++ CameraSwiftUI/CameraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSwiftUIApp.swift
+//  CameraSwiftUI
+//
+//  Created by 渡辺大智 on 2024/01/17.
+//
+
+import SwiftUI
+
+@main
+struct CameraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CameraSwiftUI
+//
+//  Created by 渡辺大智 on 2024/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
